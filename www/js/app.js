1b0c5ab0a36c5199e786af87d54129b497b2a681
Divyendu <divyendu.z@gmail.zom>
2014-11-14 23:26:37 +0530

    Added ng-repeat nested for category and items

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var ToDo=angular.module('ToDo', ['ionic']);
 
-.run(function($ionicPlatform) {
+ToDo.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +16,38 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+ToDo.controller('ToDoCtrl',function($scope){
+  $scope.InitLoad=function(){
+    $scope.ToDoList=[
+    {
+        "item_divider": {
+            "name": "Priority",
+            "icon": "ion-fireball",
+            "items": [
+                {
+                    "item": "AITEM1"
+                },
+                {
+                    "item": "AITEM2"
+                }
+            ]
+        }
+    },
+    {
+        "item_divider": {
+            "name": "List",
+            "icon": "ion-pound",
+            "items": [
+                {
+                    "item": "BITEM1"
+                },
+                {
+                    "item": "BITEM2"
+                }
+            ]
+        }
+    }];
+  };
+});
