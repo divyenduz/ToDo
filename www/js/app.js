537fd8cb002e4c555ed7bfa32e51eea0464d9a47
Divyendu <divyendu.z@gmail.com>
2015-01-16 00:54:02 +0530

    limit title to 30 char

--- www/js/app.js
+++ www/js/app.js
@@ -17,6 +17,29 @@ ToDo.factory('$localstorage', ['$window', function($window) {
   }
 }]);
 
+ToDo.directive('charLimit', function(){
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attributes){
+      var limit = $attributes.charLimit;
+      $element.bind('keyup', function(event){
+        var element = $element.parent().parent();
+        element.toggleClass('warning', limit - $element.val().length <= 10);
+        element.toggleClass('error', $element.val().length > limit);
+      });
+      $element.bind('keypress', function(event){
+        // Once the limit has been met or exceeded, prevent all keypresses from working
+        if ($element.val().length >= limit){
+          // Except backspace
+          if (event.keyCode != 8){
+            event.preventDefault();
+          }
+        }
+      });
+    }
+  };
+});
+
 ToDo.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
